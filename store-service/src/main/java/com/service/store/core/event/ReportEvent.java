c7bb5e6d4fbb4fbdf111a65293ff1281b1cc83e3
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-12-15 18:47:55 +0700

    Delete store-service directory

--- store-service/src/main/java/com/service/store/core/event/ReportEvent.java
+++
@@ -1,9 +0,0 @@
-package com.service.store.core.event;
-
-import lombok.Data;
-
-@Data
-public class ReportEvent {
-    private  String storeId;
-    private  int report;
-}
--- store-service/src/main/java/com/service/store/query/FindStoreQuery.java
+++
@@ -1,4 +0,0 @@
-package com.service.store.query;
-
-public class FindStoreQuery {
-}
