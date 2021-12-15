9f4a93c9865242a35fa69e2e684e62cfdea1818e
Thanawat080 <56470827+Thanawat080@users.noreply.github.com>
2021-12-15 18:47:20 +0700

    Delete login-service directory

--- login-service/src/main/java/com/example/demo/core/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.core.data;
-
-import com.example.demo.core.UserEntity;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<UserEntity, String> {
-    UserEntity findByUsernameAndPassword(String username, String password);
-}
